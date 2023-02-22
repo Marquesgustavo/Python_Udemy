9e6ed03b1a729c4db4416167dd85507f0c80d89e
Marquesgustavo <marquesggusatvo2@gmail.com>
2023-02-21 21:04:31 -0300

    att de projeto

---
+++ python_operadoresRelacionais.py
@@ -0,0 +1,19 @@
+maior = 2 > 1
+maior_ou_igual = 5>=5
+menor = 10 < 50
+menor_ou_igual = 2 <= 2
+igual = 'a' == 'a'
+diferente = 'a' != 'b'
+
+print(maior)
+print("''''''''''''''''''''''''''''")
+print(maior_ou_igual)
+print("''''''''''''''''''''''''''''")
+print(menor)
+print("''''''''''''''''''''''''''''")
+print(menor_ou_igual)
+print("''''''''''''''''''''''''''''")
+print(igual)
+print("''''''''''''''''''''''''''''")
+print(diferente)
+print("''''''''''''''''''''''''''''")
