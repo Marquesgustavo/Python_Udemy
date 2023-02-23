1cf5bcf12e8d9775cd44e2e64d6051bb92b19962
Marquesgustavo <marquesggusatvo2@gmail.com>
2023-02-22 22:11:42 -0300

    formatações de strings

---
+++ python_In_e_NotIn.py
@@ -0,0 +1,8 @@
+#Operaodores in e not In
+# Strings são interáveis
+
+nome = 'Vanessa'
+print(nome[2])
+
+print('a' in nome) #testando se a esta dentro da variavel nome, retorna True ou False.
+print('zero' not in nome) #invesão de saídas.
---
+++ python_NOT.py
@@ -0,0 +1,5 @@
+#Operador Lógico NOT
+#Usado para inverter expressões
+
+print(not True) #False
+print(not False) #True
